1ff22e2e50cba5f241338bd6fee3799088b5307a
vifabionomatokko5-collab <vifabionomatokko5@gmail.com>
2026-09-04 19:14:58 -0300

    Arrumar um negócio que eu deixei passar

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-onst express = require('express');
+const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mercadopago = require('mercadopago');
